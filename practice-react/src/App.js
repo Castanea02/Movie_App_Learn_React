74e3234813c067e693db4b44bcce78d0a15e2e8a
Castanea02 <ahrhk2000@gmail.com>
2023-12-30 23:37:35 +0900

    Effect

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,11 +1,47 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("All");
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes.");
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when counter changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when counter & keyword changes.");
+  }, [keyword, counter]);
+
   return (
-    <div>
+    <div align="center">
+      <input
+        type="text"
+        placeholder="Search Here"
+        onChange={onChange}
+        value={keyword}></input>
+
       <h1 className={styles.title}>Practice React</h1>
-      <Button text={"asd"} />
+      <h2>{counter}</h2>
+      <button className={styles.btn} onClick={onClick}>
+        Click me
+      </button>
     </div>
   );
 }
